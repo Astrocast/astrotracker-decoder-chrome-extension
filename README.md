b041f9c6bb3062d2915c7da4092072ce97ed32a0
Florian George <fgeo@hotmail.com>
2024-03-21 12:58:59 +0100

    Add AstroTracker v1 protocol (#1)
    * Add support for new AstroTracker v1 versioned protocol which adds altitude, accuracy, heading and ground speed and improved packing.
    * Fixed hard-coded test data used instead of actual message.
    * Now display decoded message in Text element.
    Use interpolated strings to build HTML.
    * Updated screenshots of README.md.
    * Increment extension version to "0.2.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # astrotracker-decoder-chrome-extension
-This is a browser extension to decode payload sent by Astrotracker devices into Astrocast portal.
+This is a browser extension to decode payload sent by AstroTracker devices into Astrocast portal.
 
 ## Installation
 1. Clone this repository.
@@ -13,23 +13,31 @@ This is a browser extension to decode payload sent by Astrotracker devices into 
 2. Open `Add-ons debugging tool` at `about:debugging#/runtime/this-firefox`.
 3. Click `Load Temporary Add-on` and select `manifest.json` file in the extension folder of the cloned repository.
 4. Open `Add-on manager` at `about:addons`.
-5. Give permissions to AstrotrackerDecoder extension to access your data for sites in the *://portal.astrocast.com domain.
+5. Give permissions to AstroTrackerDecoder extension to access your data for sites in the *://portal.astrocast.com domain.
 
 ## Usage
 - The extension will automatically decode Base64 data in Astrocast portal (https://portal.astrocast.com/Message/Details/*).
-- The extension will try to decode any payload which is either 6 or 20 bytes long, even if it isn't an Astrotracker device.
+- The extension will try to decode any payload which is either 6 or 20 bytes long, even if it isn't an AstroTracker device.
 
 ## Current decoded protocols
 
-### Astrotracker Protocol V1 (6 bytes)
+### AstroTracker Protocol v1 (>=10 bytes)
 
-|Byte offset|Type|Length (Bytes)|Name|Description|
-|---|---|---|---|---|
-|0|int8|1|Temperature|Temperature of the tracker in deg C (1 deg resolution)|
-|1|uint8|1|V Bat|Voltage of the battery in the tracker (multiplied by 10)|
-|2|uint32|4|Timestamp|Epoch of the measurement in UNIX time|
+|Byte offset|Type|Length (Bytes)|Name|Description|Unit|Min|Max|
+|-|-----|-|-----------|---------------------------------|----|--|--|
+|0|uint8|1|Type       |Type of message                  |    |=0x11|
+|1|uint8|1|Battery    |Battery voltage                  |mV  |3000  |5550 |
+|2|int8 |1|Temperature|Temperature                      |°C  |-128  |127  |
+|3|uint8|1|SIV        |Number of GNSS satellites in view|    |0     |255  |
+|4|int16|2|Altitude   |Height above mean sea level      |m   |-32768|32767|
+|6|uint8|1|Accuracy   |Accuracy range of the geolocation|m   |<10   |>2550|
+|7|uint8|1|Heading    |Heading of motion                |°   |0     |358  |
+|8|uint8|1|Speed      |Ground speed                     |km/h|0     |255  |
+|9|uint8|1|Switch     |State of status switch           |    |0     |1    |
 
-### Astrotracker Protocol V2 (20 bytes)
+![AstroTracker Protocol v1](screenshots/screenshot_v1.png)
+
+### Legacy AstroTracker Protocol v0.2 (20 bytes)
 
 |Byte offset|Type|Length (Bytes)|Name|Description|
 |---|---|---|---|---|
@@ -42,10 +50,14 @@ This is a browser extension to decode payload sent by Astrotracker devices into 
 |18|uint8|1|V Bat|Voltage of the battery in the tracker (multiplied by 10)|
 |19|int8|1|Temperature|Temperature of the tracker in deg C (1 deg resolution)|
 
-## Screenshots
+![AstroTracker Protocol v0.2](screenshots/screenshot_v0.2.png)
 
-### Astrotracker Protocol V1 (6 bytes)
-![Astrotracker Protocol V1](screenshots/screenshot_v1.png)
+### Legacy AstroTracker Protocol v0.1 (6 bytes)
 
-### Astrotracker Protocol V2 (20 bytes)
-![Astrotracker Protocol V2](screenshots/screenshot_v2.png)
+|Byte offset|Type|Length (Bytes)|Name|Description|
+|---|---|---|---|---|
+|0|int8|1|Temperature|Temperature of the tracker in deg C (1 deg resolution)|
+|1|uint8|1|V Bat|Voltage of the battery in the tracker (multiplied by 10)|
+|2|uint32|4|Timestamp|Epoch of the measurement in UNIX time|
+
+![AstroTracker Protocol v0.1](screenshots/screenshot_v0.1.png)
--- extension/decoder.js
+++ extension/decoder.js
@@ -3,7 +3,7 @@ function base64ToBase16(str) {
   const raw = window.atob(str);
   let result = "";
   let i = 0;
-  for (i = 0; i < raw.length; i+=1) {
+  for (i = 0; i < raw.length; i += 1) {
     const hex = raw.charCodeAt(i).toString(16);
     result += (hex.length === 2 ? hex : "0" + hex);
   }
@@ -16,51 +16,71 @@ const base64_element = document.getElementById("base64");
 // Decode base64 payload data
 const data_encoded = base64_element.innerText;
 const data_decoded = base64ToBase16(data_encoded);
+const bytes = [...window.atob(data_encoded)].map(c => c.charCodeAt(0));
 
-let temp = 0;
-let batt = 0;
-let date = 0;
-let slot = 0;
-let lat = 0;
-let lon = 0;
-let siv = 0;
-let gspeed = 0;
+let messageHtml = '';
 
-if ( data_decoded.length === 12 ) {
-  // Astrotracker Protocol V1
-  temp = Number.parseInt(data_decoded.slice(0, 2), 16);
-  batt = Number.parseInt(data_decoded.slice(2, 4), 16) / 10;
-  date = new Date(Number.parseInt(data_decoded.slice(4,12).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16)*1000).toUTCString();
+// AstroTracker v1 protocol - between 10 and 13 bytes - versioned 0x11
+if (bytes.length >= 10 && bytes.length <= 13 && bytes[0] == 0x11) {
+  let batt = (bytes[1] * 10) + 3000;
+  let temp = bytes[2] << 24 >> 24; // signed 8-bit
+  let siv = bytes[3];
+  let altitude = ((bytes[5] << 8) | bytes[4]) << 16 >>  16; // signed 16-bit little endian
+  let accuracy = (bytes[6] + 1) * 10;
+  let heading = bytes[7] * 2;
+  let gspeed = bytes[8];
 
   // Update DOM element's inner HTML with decoded data
-  base64_element.innerHTML += " \
-    <br /><br /><b>Astrotracker Protocol V1</b> \
-	<br />------------------------ \
-    <br />Date = " + date + " \
-    <br />Battery voltage = " + batt + "V \
-    <br />Temperature = " + temp + "°C";
+  messageHtml = "<b>AstroTracker Protocol v1</b>" +
+    "<br />------------------------" +
+    `<br />Battery = ${batt} mV` +
+    `<br />Temperature = ${temp} °C` +
+    `<br />Satellites in view = ${siv}` +
+    `<br />Altitude = ${altitude} m` +
+    `<br />Accuracy = <${accuracy} m` +
+    `<br />Heading = ${heading}°` +
+    `<br />Ground speed = ${gspeed} km/h` +
+    `<br />State = ${bytes[9]}`
+  // Raw value of optional reserved bytes
+  for (i = 10; i < bytes.length; i++)
+    messageHtml += `<br />Reserved${i - 9} = ${'0x' + bytes[i].toString(16).padStart(2, '0')} (${bytes[i]})`;
 
-} else if ( data_decoded.length === 40 ) {
-  // AstroTracker protocol V2 - LOGGER_TAG_PVT_SLOT
-  slot = Number.parseInt(data_decoded.slice(0, 2), 16);
-  date = new Date(Number.parseInt(data_decoded.slice(2,10).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16)*1000).toUTCString();
-  lat = Number.parseInt(data_decoded.slice(10,18).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16) * 1e-07;
-  lon = Number.parseInt(data_decoded.slice(18,26).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16) * 1e-07;
-  siv = Number.parseInt(data_decoded.slice(26, 28), 16);
-  gspeed = Number.parseInt(data_decoded.slice(28,36).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16);
-  batt = Number.parseInt(data_decoded.slice(36, 38), 16) / 10;
-  temp = Number.parseInt(data_decoded.slice(38, 40), 16);
+// AstroTracker Protocol v0.1 - 6 bytes
+} else if (data_decoded.length === 12) {
+  let temp = Number.parseInt(data_decoded.slice(0, 2), 16);
+  let batt = Number.parseInt(data_decoded.slice(2, 4), 16) / 10;
+  let date = new Date(Number.parseInt(data_decoded.slice(4, 12).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16) * 1000).toISOString();
+
+  messageHtml = "<b>AstroTracker Protocol v0.1</b>" +
+    "<br />--------------------------" +
+    `<br />Date = ${date}` +
+    `<br />Battery = ${batt} V` +
+    `<br />Temperature = ${temp} °C`;
+
+// AstroTracker protocol v0.2 - 20 bytes LOGGER_TAG_PVT_SLOT
+} else if (data_decoded.length === 40) {
+  let slot = Number.parseInt(data_decoded.slice(0, 2), 16);
+  let date = new Date(Number.parseInt(data_decoded.slice(2, 10).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16) * 1000).toISOString();
+  let lat = Number.parseInt(data_decoded.slice(10, 18).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16) * 1e-07;
+  let lon = Number.parseInt(data_decoded.slice(18, 26).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16) * 1e-07;
+  let siv = Number.parseInt(data_decoded.slice(26, 28), 16);
+  let gspeed = Number.parseInt(data_decoded.slice(28, 36).match(/[a-fA-F0-9]{2}/g).reverse().join(""), 16);
+  let batt = Number.parseInt(data_decoded.slice(36, 38), 16) / 10;
+  let temp = Number.parseInt(data_decoded.slice(38, 40), 16);
 
   // Update DOM element's inner HTML with decoded data
-  base64_element.innerHTML += " \
-    <br /><br /><b>Astrotracker Protocol V2</b> \
-	<br />------------------------ \
-    <br />Slot tag = " + slot + " \
-    <br />Date = " + date + " \
-    <br />Latitude = " + lat + " \
-    <br />Longitude = " + lon + " \
-    <br />Satellites in view = " + siv + " \
-    <br />Ground speed = " + gspeed + "km/h \
-    <br />Battery voltage = " + batt + "V \
-    <br />Temperature = " + temp + "°C";
+  messageHtml = "<b>AstroTracker Protocol v0.2</b>" +
+    "<br />--------------------------" +
+    `<br />Slot tag = ${slot}` +
+    `<br />Date = ${date}` +
+    `<br />Latitude = ${lat}` +
+    `<br />Longitude = ${lon}` +
+    `<br />Satellites in view = ${siv}` +
+    `<br />Ground speed = ${gspeed} km/h` +
+    `<br />Battery = ${batt} V` +
+    `<br />Temperature = ${temp} °C`;
 }
+
+// Replace "Text" element's inner HTML with decoded data
+if (messageHtml)
+  document.getElementById("text").innerHTML = messageHtml;
